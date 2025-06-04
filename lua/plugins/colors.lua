068f310f4f6809bcc51ba331b7fd2a97413663e0
asakshat <asakshat453@gmail.com>
2025-06-04 22:53:13 +0200

    added vim tmux nav

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -32,6 +32,8 @@ return {
 		priority = 1000, -- load before other plugins
 		config = function()
 			vim.cmd("colorscheme carbonfox")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
 	},
 
